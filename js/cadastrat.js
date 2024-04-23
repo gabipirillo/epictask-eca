06a441e7c12381a9a5c2bd8c56bc657a5a89fa52
Amandinha0liveira <amandaalmi3@gmail.com>
2024-04-22 21:54:41 -0300

    validação do cadastro

--- js/cadastrat.js
+++ js/cadastrat.js
@@ -19,21 +19,31 @@ function validar (tarefa){
     if (tarefa.titulo.trim() == ""){
         document.querySelector("#titulo").classList.add("is-error")
         document.querySelector("#titulo-erro").innerText = "Nome do produto é obrigatorio"
+        console.log("Tópico Reprovado. Insira os dados corretamente");
+    } else{
+        limparError();
+        console.log("Tópico Aprovado");
     }
     if (tarefa.descricao.trim() == "" || tarefa.descricao.lenght <10){
         document.querySelector("#descricao").classList.add("is-error")
         document.querySelector("#descricao-erro").innerText = "Descrição deve ter pelo menos 10 caracteres"
+        console.log("Tópico Reprovado. Insira os dados corretamente");
+    }else{
+        limparError();
+        console.log("Tópico Aprovado");
     }
     if (tarefa.pontos <=0){
         document.querySelector("#pontos").classList.add("is-error")
         document.querySelector("#pontos-erro").innerText = "pontos deve ser maior que zero "
+        console.log("Tópico Reprovado. Insira os dados corretamente");
+    }else{
+        limparError();
+        console.log("Tópico Aprovado");
     }
 }
 
-// function limparError(){
-//     //Todo percorrer o array
-//     document.querySelectorAll("input.is-error, textarea .is-error")
-//     .classList
-//     .remove("is-error")
-
-// }
+function limparError(){
+    //Todo percorrer o array
+    const errorElements = document.querySelectorAll("input.is-error, textarea.is-error")
+    errorElements.forEach(element => element.classList.remove("is-error"))
+}
